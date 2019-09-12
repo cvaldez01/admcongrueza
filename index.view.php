8e8ab720b0634550c05501f08bed06dbb9e9ed81
cvaldez01 <55217386+cvaldez01@users.noreply.github.com>
2019-09-11 21:24:17 -0400

    Add files via upload

---
+++ index.view.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+
+<!--Enlaces-->
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+<script src="https://kit.fontawesome.com/0e62d22636.js"></script>
+<link rel="stylesheet" href="estilos.css" type="text/css">
+<script src="script.js"></script>
+
+<!--Barra de Login y Registro-->
+
+
+    <div id="menu-sup">
+      <ul class="">
+        <li><a href="#">Login</a></li>
+        <li><a href="#">Registrate</a></li>
+      </ul>
+    </div>
+
+<!--Banner dinamico-->
+<div id="container-1">
+  <div id="banner1">
+    <h2>Banner dinamico</h2>
+  </div>
+</div>
+
+    <title>Admcongrueza</title>
+  </head>
+  <body>
+
+<div id="jumbotron">
+  <!--Static Banner-->
+  <div id="seccion-1">
+    <h2>Tu <span>meta</span> inmobiliaria es nuestra <span>meta</span></h2>
+    <h3>Que Buscas?</h3>
+    <ul class="sec-l-1">
+      <li class="sec-1"><a href="#">Rentar </a></li>
+      <li class="sec-1"><a href="#">Administracion de Condominios </a></li>
+      <li class="sec-1"><a href="#">Comprar</a></li>
+      <li class="sec-1"><a href="#">Vender</a></li>
+    </ul>
+
+  </div>
+
+  <div id="seccion-2">
+    <h2>Hazte parte de nuestra comunidad</h2>
+    <h3>Beneficios</h3>
+    <ul>
+           <li class="sec-2"><a href="">Flotas Telefonicas</a></li>
+           <li class="sec-2"><a href="#">Pagos de Servicios</a></li>
+           <li class="sec-2"><a href="#">Informe mensuales(Ingresos y Gastos)</a></li>
+           <li class="sec-2"><a href="#">Mucho Mas...</a></li>
+    </ul>
+
+  </div>
+
+  <div id="seccion-3">
+    <h2>Seguridad</h2>
+    <ul>
+      <li class="sec-3"><a href="#">Camaras de Vigilancia(Exclusivas)</a></li>
+      <li class="sec-3"><a href="#">Servicios de Monitoreo</a></li>
+      <li class="sec-3"><a href="#">Guardian de Seguridad</a></li>
+      <li class="sec-3"><a href="#">Mucho Mas...</a></li>
+    </ul>
+
+  </div>
+
+  <div id="seccion-4">
+    <h2>Mantenimiento y Remodelacion </h2>
+    <ul>
+      <li class="sec-1"><a href="#">Apartamentos</a></li>
+      <li class="sec-1"><a href="#">Negocios </a></li>
+      <li class="sec-1"><a href="#">Viviendas</a></li>
+      <li class="sec-1"><a href="#">Otras propiedades </a></li>
+    </ul>
+
+  </div>
+
+  <div>
+
+  </body>
+
+
+  <footer></footer>
+</html>
